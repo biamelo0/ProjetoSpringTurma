fc8ea45632cf9357b9f8ff14a9b99479f986b791
biamelo0 <beatrizmelo0952@gmail.com>
2024-04-25 16:40:45 -0300

    projetospring

---
+++ src/main/java/projetojpaAvaliacao/repository/AlunoRepository.java
@@ -0,0 +1,26 @@
+package projetojpaAvaliacao.repository;
+
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import projetojpaAvaliacao.entities.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno,Long>{
+	List<Aluno> findByCidade(String cidade);
+	List<Aluno> findByRenda(double renda);
+	List<Aluno> findByra(String ra);
+	List<Aluno> findByCidadeAndRenda (String cidade, double renda);
+	
+	@Query("SELECT a FROM Aluno a WHERE a.nome = :nome")
+	List<Aluno>findByNome(@Param("nome")String nome);
+	
+	@Query("SELECT a FROM Aluno a WHERE a.nomeCompleto = :nomeCompleto")
+	List<Aluno>findByNomeLike(@Param("nomeCompleto")String nomeCompleto);
+	
+	@Query("SELECT a FROM Aluno a WHERE a.nomeCompleto = :turmaId")
+	List<Aluno>findByTurmaId(@Param("turmaId")String turmaId);
+	
+}
